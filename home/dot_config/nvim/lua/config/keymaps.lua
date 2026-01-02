bebb33eb0bb04f27ceb5577fc5b8bfc70f7533f8
Forgo7ten <Forgo7ten2020@gmail.com>
2026-01-03 01:25:37 +0800

    feat: update nvim keymaps

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 从 terminal模式 到 normal模式
+vim.keymap.set("t", "jj", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+-- 从 insert模式 到 normal模式
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
