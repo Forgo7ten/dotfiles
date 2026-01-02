436fa4de2ec9c02e115da7b9b8b83b051af2ae29
Forgo7ten <Forgo7ten2020@gmail.com>
2026-01-03 00:31:44 +0800

    feat: add vim options.

---
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          -- 影响 <leader>e 或 <leader>E (文件浏览器/文件树)
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+      -- dashboard 初始化界面
+      dashboard = {
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "terminal",
+            cmd = "colorscript -e square",
+            height = 5,
+            padding = 1,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
+    },
+  },
+}
