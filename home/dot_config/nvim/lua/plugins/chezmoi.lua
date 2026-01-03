fffd2fadee4775ea4d4341eddeba1d8c22a59a86
Forgo7ten <Forgo7ten2020@gmail.com>
2026-01-03 14:39:46 +0800

    feat: add nvim chezmoi.nvim options

---
+++ home/dot_config/nvim/lua/plugins/chezmoi.lua
@@ -0,0 +1,9 @@
+return {
+  -- {
+  --   "xvzc/chezmoi.nvim",
+  --   init = function() 
+  --     -- 禁用自动监控文件更改并chezmoi apply
+  --     vim.notify("Chezmoi 自动应用已禁用", vim.log.levels.INFO, { title = "chezmoi.lua" })
+  --   end,
+  -- },
+}
